91aacf76d1b77d2fca881d63f3263ae14e7ccd93
Denis <dmgburg@gmail.com>
2016-01-31 10:00:18 +0300

    fixed get ks error handling

--- src/main/groovy/com/dmgburg/dozor/web/BotController.groovy
+++ src/main/groovy/com/dmgburg/dozor/web/BotController.groovy
@@ -1,14 +1,18 @@
 package com.dmgburg.dozor.web
 
 import com.dmgburg.dozor.core.Application
+import groovy.util.logging.Slf4j
 import org.springframework.stereotype.Controller
 
 @Controller
+@Slf4j
 class BotController {
     private final Application app
 
     BotController() {
         app = new Application()
+        println "Going to start Bot application"
+        log.info("Going to start Bot application")
         app.run()
     }
 }
